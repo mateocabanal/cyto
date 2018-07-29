2adbf77c88b23e187ec2bf43bb74904716f753b5
oetam5002 <mateo200525@gmail.com>
2018-07-29 08:45:31 -0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -110,7 +110,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "FF9507CA55455F37
 
 const char* const SEED_NODES[] = { 
   "108.172.131.200:18000",
-  "108.172.131.200:18000",
+  "108.172.131.200:18002",
 };
 
 struct CheckpointData {
